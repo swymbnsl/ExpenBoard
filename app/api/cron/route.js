4cc77893c15e288d8a421459fa6b24acacad566c
Swayam Bansal <swayambansal@outlook.com>
2024-07-01 09:33:47 +0530

    add vercel.json

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -6,19 +6,23 @@ connect()
 
 export async function GET() {
   try {
-    await User.deleteMany({
-      $and: [{ isVerified: true }, { verifyTokenExpiry: { $lt: Date.now() } }],
+    const delRes = await User.deleteMany({
+      $and: [{ isVerified: false }, { verifyTokenExpiry: { $lt: Date.now() } }],
     })
 
     return NextResponse.json(
-      { message: "Users deleted successfully", success: true },
+      {
+        message: "Users deleted successfully",
+        success: true,
+        delData: delRes,
+      },
       { status: 200 }
     )
   } catch (error) {
     return NextResponse.json(
       {
         message: "Could not delete users",
-        error: error.message,
+        error: error,
         success: false,
       },
       { status: 500 }
