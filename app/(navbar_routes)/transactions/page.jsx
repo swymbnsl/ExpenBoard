aec9de68bc000463b0fe390d406d5e0420c0062c
Swayam Bansal <swayambansal@outlook.com>
2024-07-16 10:35:01 +0530

    No redux for now

--- app/(navbar_routes)/transactions/page.jsx
+++ app/(navbar_routes)/transactions/page.jsx
@@ -1,9 +1,52 @@
 "use client"
 
 import { DatePickerWithRange } from "@/components/shared/date_range_picker"
-import React from "react"
+import React, { useCallback, useState } from "react"
+import { set } from "date-fns"
 
 export default function Transactions() {
+  const [date, setDate] = useState({
+    from: new Date(
+      new Date(new Date().setDate(new Date().getDate() - 30)).setHours(
+        0,
+        0,
+        0,
+        0
+      )
+    ),
+    to: new Date(),
+  })
+  const [displayDate, setDisplayDate] = useState({
+    from: new Date(
+      new Date(new Date().setDate(new Date().getDate() - 30)).setHours(
+        0,
+        0,
+        0,
+        0
+      )
+    ),
+    to: new Date(),
+  })
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleOpen = useCallback((open) => {
+    setIsOpen(open)
+    if (!open && displayDate && displayDate.to && displayDate.from) {
+      setDate((prevRange) => {
+        return {
+          ...prevRange,
+          ["from"]: displayDate.from,
+          ["to"]: set(displayDate.to, {
+            hours: 23,
+            minutes: 59,
+            seconds: 59,
+            milliseconds: 999,
+          }),
+        }
+      })
+    }
+  })
+
   return (
     <>
       <div className="w-full h-full justify bg-center items-center">
@@ -13,7 +56,13 @@ export default function Transactions() {
           </span>{" "}
         </div>
         <div className="p-3">
-          <DatePickerWithRange />
+          <DatePickerWithRange
+            handleOpen={handleOpen}
+            date={date}
+            isOpen={isOpen}
+            displayDate={displayDate}
+            setDisplayDate={setDisplayDate}
+          />
         </div>
       </div>
     </>
