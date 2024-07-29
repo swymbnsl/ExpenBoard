447fb5ffb8b25798d2ae28ad35ed0d55bad99c60
Swayam Bansal <swayambansal@outlook.com>
2024-07-29 13:10:13 +0530

    Settings page profile component completed

--- app/(navbar_routes)/settings/page.jsx
+++ app/(navbar_routes)/settings/page.jsx
@@ -1,5 +1,18 @@
-import React from "react"
+"use client"
+
+import React, { useContext } from "react"
+import Profile from "./profile"
+import { UserDetailsContext } from "@/context/userDetails"
 
 export default function Settings() {
-  return <div>Settings</div>
+  const { name, pfp, email } = useContext(UserDetailsContext)
+
+  return (
+    <div className="w-full flex flex-col items-center">
+      <div className="p-3 w-full ">
+        <span className="text-themeonsurface font-bold text-2xl">Settings</span>
+      </div>
+      <Profile name={name} pfp={pfp} email={email} />
+    </div>
+  )
 }
---
+++ app/(navbar_routes)/settings/profile.jsx
@@ -0,0 +1,47 @@
+import { Avatar, Skeleton } from "@mui/material"
+import { blue } from "@mui/material/colors"
+import { Pencil } from "lucide-react"
+import React from "react"
+
+export default function Profile({ name, pfp, email }) {
+  return (
+    <div className="p-3 w-[100%] h-full flex justify-between items-center">
+      <div className="flex justify-center items-center gap-5">
+        {name ? (
+          <Avatar sx={{ bgcolor: blue[500], width: 60, height: 60 }} src={pfp}>
+            {!pfp && name[0]}
+          </Avatar>
+        ) : (
+          <Skeleton variant="circular" width={60} height={60} />
+        )}
+
+        {name && email ? (
+          <div className="flex flex-col">
+            <span className="text-themeonsurface text-xl font-semibold">
+              {name.length <= 10 ? name : name.slice(0, 10) + "..."}
+            </span>
+            <span className="text-sm font-semibold text-themeonsurfacevar break-words">
+              {email.length <= 24 ? email : email.slice(0, 21) + "..."}
+            </span>
+          </div>
+        ) : (
+          <div className="flex flex-col gap-1">
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              width={140}
+              height={20}
+            />
+            <Skeleton
+              animation="wave"
+              variant="rounded"
+              width={180}
+              height={14}
+            />
+          </div>
+        )}
+      </div>
+      <Pencil />
+    </div>
+  )
+}
