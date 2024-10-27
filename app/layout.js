c7d7fbfe089251b58cd9db6c1042e98c65a3c085
Swayam Bansal <swayambansal@outlook.com>
2024-10-27 23:26:02 +0530

    Make the website accessible for mobile devices only

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,10 @@
 import { Roboto } from "next/font/google"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme_providers/shadcn-theme-provider"
-
 import { CssBaseline } from "@mui/material"
 import { MuiThemeProvider } from "@/components/theme_providers/material-ui-theme-provider"
-
 import { Toaster } from "react-hot-toast"
+import MobileOnlyWrapper from "@/components/MobileOnlyWrapper" // Add this import
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -25,7 +24,7 @@ export default function RootLayout({ children }) {
           <MuiThemeProvider>
             <CssBaseline />
             <Toaster />
-            {children}
+            <MobileOnlyWrapper>{children}</MobileOnlyWrapper>
             <Toaster richColors />
           </MuiThemeProvider>
         </ThemeProvider>
